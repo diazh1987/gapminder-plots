0f5e6267361bed26893dd7a609005324b5c98faa
Giovanni Diaz <diazh1987@gmail.com>
2017-08-06 10:35:14 -0700

    take log of gdPercap vs lifeExp

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -4,5 +4,6 @@ gapminder <- read.csv("data/gapminder-FiveYears.csv")
 
 library(ggplot2)
 ggplot(gapminder, aes(x = gdpPercap , y = lifeExp)) + 
-        geom_point(aes(color = continent, shape=continent))
+        geom_point(aes(color = continent, shape=continent)) + 
+        scale_x_log10()
 
